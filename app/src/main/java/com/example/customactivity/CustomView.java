868aeb913d3b49b9d094761ef5db6577176cd698
vasilio13 <vasilkhaliaukin@gmail.com>
2020-04-08 11:50:53 +0300

    problem onDraw

--- app/src/main/java/com/example/customactivity/CustomView.java
+++ app/src/main/java/com/example/customactivity/CustomView.java
@@ -5,9 +5,13 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
+
+import java.util.Random;
 
 public class CustomView extends View {
 
@@ -60,9 +64,16 @@ public class CustomView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         int size = SIZE / 2;
-        canvas.drawRect(centerX - size, centerY - size, centerX + size, centerY + size, paint);
+        canvas.drawCircle(centerX - size, centerY - size, SIZE*4, paint);
+        initPaintBlue();
+        canvas.drawCircle(centerX - size, centerY - size, SIZE*2, paint);
+        initPaintRandom();
+        canvas.drawArc(centerX-size-100,centerY-size-100,centerX-size+100,centerY-size+100, 0,90,true,paint);
+
         super.onDraw(canvas);
     }
+
+
 
     private void initPaint() {
         paint = new Paint(Paint.ANTI_ALIAS_FLAG);
@@ -71,5 +82,24 @@ public class CustomView extends View {
         paint.setStyle(Paint.Style.FILL);
     }
 
+    private void initPaintBlue() {
+        paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        paint.setAntiAlias(true);
+        paint.setColor(Color.BLUE);
+        paint.setStyle(Paint.Style.FILL);
+    }
 
-}
+    private void initPaintRandom() {
+        Random rnd = new Random();
+        int rndcolor = Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256));
+        paint.setColor(rndcolor);
+    }
+
+    public void onTouch(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+initPaintRandom();
+
+        }
+    }
+
+    }
--- app/src/main/java/com/example/customactivity/CustomViewActivity.java
+++ app/src/main/java/com/example/customactivity/CustomViewActivity.java
@@ -3,6 +3,7 @@ package com.example.customactivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
